cd08f4115e99df3fa21f6b9e0ef0570d9224435c
Joanas01 <joana.s@mail.ugm.ac.id>
2023-10-25 19:15:52 +0700

    firts push

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+function sound() {
+  var snd = new Audio('../assets/quack.mp3')//wav is also supported
+  snd.play()//plays the sound
+}
+
+function onSubmit() {
+  let name = document.forms["myForm"]["name"].value;
+  let birthdate = document.forms["myForm"]["date"].value;
+  let pesan = document.forms["myForm"]["message"].value;
+  let gender = document.forms["myForm"]["gender"].value;
+  let form = document.forms["myForm"]
+  console.log(form);
+
+  if(name==""|| date==""|| gender=="" ||message==""){
+    alert("tidak boleh ada form yang kosong");
+    return false;
+  }
+
+  else{
+  let display = document.getElementById("submitdisplay")
+  display.innerHTML = `<b>Message sent!</b></br> <b>Nama:</b>${name}</br> <b>Tanggal Lahir:</b>${birthdate}</br> <b>Jenis Kelamin:</b>${gender}</br> <b>Pesan:</b>${pesan}`
+}}
