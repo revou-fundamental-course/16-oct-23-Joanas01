1813871a94ec483da4956268c61c7e712eb6d78b
Joanas01 <148172080+Joanas01@users.noreply.github.com>
2023-10-26 22:52:10 +0700

    script.js

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 function sound() {
-  var snd = new Audio('../assets/quack.mp3')
-  snd.play()
+  var snd = new Audio('../assets/quack.mp3')//wav is also supported
+  snd.play()//plays the sound
 }
 
 function onSubmit() {
@@ -11,7 +11,7 @@ function onSubmit() {
   let form = document.forms["myForm"]
   console.log(form);
 
-  if(name==""|| birthdate==""|| gender=="" ||pesan==""){
+  if(name==""|| date==""|| gender=="" ||message==""){
     alert("tidak boleh ada form yang kosong");
     return false;
   }
@@ -20,3 +20,25 @@ function onSubmit() {
   let display = document.getElementById("submitdisplay")
   display.innerHTML = `<b>Message sent!</b></br> <b>Nama:</b>${name}</br> <b>Tanggal Lahir:</b>${birthdate}</br> <b>Jenis Kelamin:</b>${gender}</br> <b>Pesan:</b>${pesan}`
 }}
+
+var slideIndex = 1;
+
+function plusDivs(n) {
+  showDivs(slideIndex += n)};
+
+function showDivs(n){
+  var i;
+  var imgList= document.getElementsByClassName("img-slideshow");
+  if (n>imgList.length) slideIndex=1;
+  else if (n < 1) slideIndex = imglist.length;
+
+  for (i=0; i<imgList.length; i++){
+    imgList[i].style.display="none";
+    }
+
+  imgList[slideIndex-1].style.display="block";
+  }
+
+setInterval(() =>{
+  plusDivs(1);
+ },3000);
