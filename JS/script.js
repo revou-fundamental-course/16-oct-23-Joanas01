2366acf6e1ec1ffbf886335e7146c54ae682fe97
Joanas01 <joana.s@mail.ugm.ac.id>
2023-10-27 09:51:03 +0700

    deploying changes

--- JS/script.js
+++ JS/script.js
@@ -20,3 +20,25 @@ function onSubmit() {
   let display = document.getElementById("submitdisplay")
   display.innerHTML = `<b>Message sent!</b></br> <b>Nama:</b>${name}</br> <b>Tanggal Lahir:</b>${birthdate}</br> <b>Jenis Kelamin:</b>${gender}</br> <b>Pesan:</b>${pesan}`
 }}
+
+var slideIndex = 1;
+
+function plusDivs(n) {
+  showDivs(slideIndex += n)};
+
+function showDivs(n){
+  var i;
+  var imgList= document.getElementsByClassName("img-slideshow");
+  if (n>imgList.length) slideIndex=1;
+  else if (n < 1) slideIndex = imglist.length;
+
+  for (i=0; i<imgList.length; i++){
+    imgList[i].style.display="none";
+    }
+
+  imgList[slideIndex-1].style.display="block";
+  }
+
+setInterval(() =>{
+  plusDivs(1);
+ },3000);
